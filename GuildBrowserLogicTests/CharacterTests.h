3a5201e735829f31103d51fc56cb48838937b73c
Andrew Weaver <andrewpweaveris@gmail.com>
2013-09-09 11:27:53 +0100

    Added unit tests (from tutorial)...

---
+++ GuildBrowserLogicTests/CharacterTests.h
@@ -0,0 +1,13 @@
+//
+//  CharacterTests.h
+//  GuildBrowser
+//
+//  Created by Andrew Weaver on 09/09/2013.
+//  Copyright (c) 2013 Charlie Fulton. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface CharacterTests : SenTestCase
+
+@end
---
+++ GuildBrowserLogicTests/WowUtilsTests.h
@@ -0,0 +1,13 @@
+//
+//  WowUtilsTests.h
+//  GuildBrowser
+//
+//  Created by Andrew Weaver on 09/09/2013.
+//  Copyright (c) 2013 Charlie Fulton. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface WowUtilsTests : SenTestCase
+
+@end
